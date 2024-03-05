94e73f5cc382475a11cbf0dc6464ec074acd8ac4
Juan Pablo Allin Cañas <juan.allin@sofka.com.co>
2024-03-05 07:41:33 -0500

    dependencies

---
+++ src/main/java/com/library/sofkau/LibrarySofkaUApplication.java
@@ -0,0 +1,13 @@
+package com.library.sofkau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySofkaUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySofkaUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/sofkau/LibrarySofkaUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.sofkau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySofkaUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
